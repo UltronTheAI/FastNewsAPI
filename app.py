a96274996431754880c24b448c37123d87810267
Swaraj Puppalwar <79976106+UltronTheAI@users.noreply.github.com>
2022-07-23 20:51:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import NewsAPI
 
-news = NewsAPI.getCountryNews('IN', 'hi')
-
-num = 0
-
-for news_data in news:
-    num += 1
-    news_data_ = news_data['news']
-    print ('# - News ' + str(num) + '\n')
-    print('Title: ' + news_data_['title'] + '\n')
-    print('Sub-Title: ' + news_data_['subTitle'] + '\n')
-    print('Link: ' + news_data_['url'] + '\n')
-    print('Provider: ' + news_data_['provider'] + '\n')
+news = NewsAPI.getCountryNews(
+                    'IN', # country
+                     'hi' # lang
+                    )
+# print(f"{news[n]['title']}")
+print(f"{news}")
